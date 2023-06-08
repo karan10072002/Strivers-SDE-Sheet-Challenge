97d5aec61f2653772532cede3851a5ea71f50d9d
Karan kumar Chaurasia <karan10072002@gmail.com>
2023-06-08 18:02:26 +0530

    Update Next permutation.py

--- Next permutation.py
+++ Next permutation.py
@@ -1,11 +1,10 @@
-class Solution(object):
-        def nextPermutation(self, nums):
-            for i in range(len(nums)-1, 0, -1):
-                if nums[i - 1] < nums[i]:
-                    nums[i:] = sorted(nums[i:])
-                i = i - 1
-                for k in range(i, len(nums)):
-                    if nums[i] < nums[k]:
-                        nums[k], nums[i] = nums[i], nums[k]
-                        return nums
-            return nums.reverse()
+def nextPermutation(self, nums):
+    for i in range(len(nums)-1, 0, -1):
+        if nums[i - 1] < nums[i]:
+            nums[i:] = sorted(nums[i:])
+        i = i - 1
+        for k in range(i, len(nums)):
+            if nums[i] < nums[k]:
+                nums[k], nums[i] = nums[i], nums[k]
+                return nums
+    return nums.reverse()
